dd95e623df87e5d1ffb43a5e30291538f6c0b93a
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-25 13:23:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+function createPromise(){
+	const delay=Math.floor(Math.random()*2000)+1000;
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve(delay/1000);
+		},delay);
+	});
+}
+const promises=[
+	createPromise(),
+	createPromise(),
+    createPromise()
+];
+const tableBody = document.getElementById('tbody');
+const loadingRow = document.getElementById('loadingRow');
+
+
+loadingRow.style.display = ''
+Promise.all(promises).then(results=>{
+	console.log('All promises resolved:', results);
+	 loadingRow.style.display = 'none';
+    tableBody.innerHTML = '';
+	results.forEach((time,index)=>{
+		const row=document.createElement('tr');
+		row.innerHTML=`
+		<td>Promise ${index + 1}</td>
+        <td>${time.toFixed(3)}</td>`;
+		tableBody.appendChild(row);
+	});
+	const totalTime=results.reduce((acc, curr) => acc + curr, 0);
+	const totalRow=document.createElement('tr');
+	  totalRow.innerHTML = `
+            <td>Total</td>
+            <td>${totalTime.toFixed(3)}</td>
+        `;
+        tableBody.appendChild(totalRow);
+}) .catch(error => {
+        console.error('Error waiting for promises:', error);
+	    tableBody.innerHTML = `<tr><td colspan="2">Error: ${error.message}</td></tr>`;
+       
+        loadingRow.style.display = 'none';
+});
+
